08c7edaaea3070c406828840f3741df0e0ba84c2
Sara Gisondi <sara.gisondi@outlook.it>
2023-04-03 19:21:21 +0200

    modify cycle for

--- js/script.js
+++ js/script.js
@@ -1,56 +1,15 @@
-
 /*******************CONTAINER*******************************/
 const container = document.querySelector('.container');
 
-/********************IMG******************************/
-// //1.
-// const img1 = document.createElement("img");
-// img1.src= "img/angela-caroll-chief-editor.jpg";
-// let src1 = document.querySelector('.img-1');
-// src1.append(img1);
-
-// //2.
-// const img2 = document.createElement("img");
-// img2.src= "img/angela-lopez-social-media-manager.jpg";
-// let src2 = document.querySelector('.img-2')
-// src2.append(img2);
-
-// //3.
-// const img3 = document.createElement("img");
-// img3.src= "img/barbara-ramos-graphic-designer.jpg";
-// let src3 = document.querySelector('.img-3');
-// src3.append(img3);
-
-// //4.
-// const img4 = document.createElement("img");
-// img4.src= "img/scott-estrada-developer.jpg";
-// let src4 = document.querySelector('.img-4');
-// src4.append(img4);
-
-// //5.
-// const img5 = document.createElement("img");
-// img5.src= "img/walter-gordon-office-manager.jpg";
-// let src5 = document.querySelector('.img-5');
-// src5.append(img5);
-
-// //6.
-// const img6 = document.createElement("img");
-// img6.src= "img/wayne-barnett-founder-ceo.jpg";
-// let src6 = document.querySelector('.img-6');
-// src6.append(img6);
-
-
-// container.append(img1,img2,img3,img4,img5,img6);
-
-// console.log(container)
-// console.log( img1,img2,img3,img4,img5,img6)
+/********************LIST******************************/
+const list = document.querySelector('ul');
 
 /**************ARRAY***********************************/
 const team = [
   //1.
   {
   nome: 'Angela Caroll',
-  ruolo: 'chief editor',
+  ruolo: 'Chief Editor',
   foto: `img/angela-caroll-chief-editor.jpg`
   },
   
@@ -90,3 +49,31 @@ const team = [
     }
 
 ]
+
+for (let i = 0; i < team.length; i++) {
+  const members = team[i];
+  console.log(members.nome, members.ruolo, members.foto);
+
+  }
+  
+
+  for (let member of team){
+    const li = document.createElement('li');
+    list.append(li);
+
+    const name = document.createElement('h1');
+    li.append(name);
+    name.innerHTML = member.nome;
+
+    const role = document.createElement('h2')
+    li.append(role);
+    role.innerHTML = member.ruolo
+
+    const img = document.createElement('img');
+    li.append(img);
+    img.src = member.foto;
+    img.innerHTML =  "<img src ='" + member.foto + " ' >"
+    
+
+  }
+  console.log(team.member)
